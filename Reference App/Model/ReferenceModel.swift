4f074670edca61ba1f21de3bf9d674e6de01d80c
DANIIL <gusarov_ib18@st.ithub.ru>
2019-03-26 16:33:50 +0300

    ij

--- Reference App/Model/ReferenceModel.swift
+++ Reference App/Model/ReferenceModel.swift
@@ -10,7 +10,7 @@ import UIKit
 
 struct ReferenceModel {
     
-    static let name = "Парусники"
+    static let name = "Counter-Strike"
     
     var image: UIImage?
     var title: String
@@ -22,24 +22,24 @@ struct ReferenceModel {
         
         return [
             ReferenceModel(
-                image: UIImage(named: "brig"),
-                title: "Бриг",
-                detail: "Двухмачтовое судно с прямым парусным вооружением фок-мачты и грот-мачты, но с одним косым гафельным парусом на гроте — грота-гаф-триселем."
+                image: UIImage(named: "1"),
+                title: "CS 1.6",
+                detail: "Многопользовательская компьютерная игра в жанре шутера от первого лица, разработанная и выпущенная для Microsoft Windows американской компанией Valve. Первоначально Counter-Strike представляла собой пользовательскую модификацию к игре Half-Life"
             ),
             ReferenceModel(
-                image: UIImage(named: "corvette"),
-                title: "Корвет",
-                detail: "Трёхмачтовый корабль водоизмещением 400—600 т с полным прямым парусным вооружением, имел до 32 орудий."
+                image: UIImage(named: "css"),
+                title: "CS Source",
+                detail: "Counter-Strike: Source (сокращается до CS: Source и CS:S) — компьютерная игра в жанре многопользовательского командного шутера от первого лица. Pемейк Counter-Strike, модификации 1999 года для игры Half-Life. Построенная на игровом движке Source, игра была выпущена в 2004 году изначально в комплекте с Half-Life 2, однако позже появились и отдельные её издания."
             ),
             ReferenceModel(
-                image: UIImage(named: "frigate"),
-                title: "Фрегат",
-                detail: "Военный трёхмачтовый корабль с полным парусным вооружением с одной или двумя (открытой и закрытой) орудийными палубами."
+                image: UIImage(named: "cs0"),
+                title: "CS Condition Zero",
+                detail: "Counter-Strike: Condition Zero — компьютерная игра в жанре трёхмерного шутера с видом от первого лица, разработанная компаниями Valve, Turtle Rock Studios, Gearbox Software, Rogue Entertainment и Ritual Entertainment. Выпущена издателем Vivendi в 2004 году; позднее изданием стала заниматься Valve."
             ),
             ReferenceModel(
-                image: UIImage(named: "warship"),
-                title: "Линейный корабль",
-                detail: "Класс парусных трёхмачтовых деревянных военных кораблей."
+                image: UIImage(named: "csg"),
+                title: "CS:GO",
+                detail: "Counter-Strike: Global Offensive (CS:GO; с англ. — «Контрудар: глобальное наступление») — многопользовательская компьютерная игра, разработанная компаниями Valve и Hidden Path Entertainment. Последняя основная игра в серии игр Counter-Strike;[4] как и все игры серии, она посвящена противостоянию террористов и подразделений специального назначения.Выход на ПК под управлением Windows, на компьютерах Mac и игровых приставках Xbox 360 и PlayStation 3, состоялся 21 августа 2012 года."
             ),
 
         ]
